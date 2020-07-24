3f7ba6fcef6d0f2461138617f9fcaa96c60bdcc0
Nils Langner <nils.langner@leankoala.com>
2020-07-24 16:54:00 +0200

    alerting

---
+++ src/Repository/Entities/AlertingChannelRepository.js
@@ -0,0 +1,72 @@
+const Repository = require('../Repository')
+
+/**
+ * This class was created by the LeanApiBundle.
+ *
+ * All changes made in this file will be overwritten by the next create run.
+ *
+ * @created 2020-07-24
+ */
+class AlertingChannelRepository extends Repository {
+
+  /**
+   * List all channels for the given project
+   *
+   * @param project
+   * @param {Object} args
+   */
+  async list(project, args) {
+    const route = { path: 'alerting/channels/{project}', method: 'GET', version: 1 }
+    const argList = Object.assign({ project }, args)
+
+    return this._connection.send(route, argList)
+  }
+
+  /**
+   * @param project
+   * @param {Object} args
+   * @param {String} args.name 
+   * @param {*} args.type 
+   * @param {Array} args.options 
+   */
+  async create(project, args) {
+    const route = { path: 'alerting/channels/{project}', method: 'POST', version: 1 }
+    const argList = Object.assign({ project }, args)
+    const requiredArguments = ['name', 'type', 'options']
+    this._assertValidArguments(requiredArguments, argList)
+
+    return this._connection.send(route, argList)
+  }
+
+  /**
+   * Delete the given channel
+   *
+   * @param project
+   * @param channel
+   * @param {Object} args
+   */
+  async delete(project, channel, args) {
+    const route = { path: 'alerting/channels/{project}/{channel}', method: 'DELETE', version: 1 }
+    const argList = Object.assign({ project, channel }, args)
+
+    return this._connection.send(route, argList)
+  }
+
+  /**
+   * @param project
+   * @param channel
+   * @param {Object} args
+   * @param {String} args.name 
+   * @param {*} args.type 
+   * @param {Array} args.options 
+   */
+  async update(project, channel, args) {
+    const route = { path: 'alerting/channels/{project}/{channel}', method: 'PUT', version: 1 }
+    const argList = Object.assign({ project, channel }, args)
+
+    return this._connection.send(route, argList)
+  }
+
+}
+
+module.exports = AlertingChannelRepository
---
+++ src/Repository/Entities/AlertingPolicyRepository.js
@@ -0,0 +1,74 @@
+const Repository = require('../Repository')
+
+/**
+ * This class was created by the LeanApiBundle.
+ *
+ * All changes made in this file will be overwritten by the next create run.
+ *
+ * @created 2020-07-24
+ */
+class AlertingPolicyRepository extends Repository {
+
+  /**
+   * List all policies for the given project
+   *
+   * @param project
+   * @param {Object} args
+   */
+  async list(project, args) {
+    const route = { path: 'alerting/policies/{project}', method: 'GET', version: 1 }
+    const argList = Object.assign({ project }, args)
+
+    return this._connection.send(route, argList)
+  }
+
+  /**
+   * @param project
+   * @param {Object} args
+   * @param {String} args.name 
+   * @param {String} args.interval 
+   * @param {Array} args.severities 
+   * @param {Number} args.channels 
+   */
+  async create(project, args) {
+    const route = { path: 'alerting/policies/{project}', method: 'POST', version: 1 }
+    const argList = Object.assign({ project }, args)
+    const requiredArguments = ['name']
+    this._assertValidArguments(requiredArguments, argList)
+
+    return this._connection.send(route, argList)
+  }
+
+  /**
+   * Delete the given policy
+   *
+   * @param project
+   * @param policy
+   * @param {Object} args
+   */
+  async delete(project, policy, args) {
+    const route = { path: 'alerting/policies/{project}/{policy}', method: 'DELETE', version: 1 }
+    const argList = Object.assign({ project, policy }, args)
+
+    return this._connection.send(route, argList)
+  }
+
+  /**
+   * @param project
+   * @param policy
+   * @param {Object} args
+   * @param {String} args.name 
+   * @param {String} args.interval 
+   * @param {Array} args.severities 
+   * @param {Number} args.channels 
+   */
+  async update(project, policy, args) {
+    const route = { path: 'alerting/policies/{project}/{policy}', method: 'PUT', version: 1 }
+    const argList = Object.assign({ project, policy }, args)
+
+    return this._connection.send(route, argList)
+  }
+
+}
+
+module.exports = AlertingPolicyRepository
