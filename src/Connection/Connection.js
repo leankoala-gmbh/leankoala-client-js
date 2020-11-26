a08896b0e30ab465f30dbd094f4b24c68182a9a5
Nils Langner <nils.langner@leankoala.com>
2020-11-26 21:08:05 +0100

    with features

--- src/Connection/Connection.js
+++ src/Connection/Connection.js
@@ -81,10 +81,11 @@ class Connection {
       this._refreshExpireTimestamp = wakeUpToken[ 'expireDate' ]
       this._accessExpireTimestamp = 0
 
-      await this.refreshAccessToken(false, args.withMemories)
+      await this.refreshAccessToken(false, args.withMemories, args.withFeatures)
 
     } else {
       let withMemories = false
+      let withFeatures = false
 
       if (!this._connectionArgs.hasOwnProperty('username')) {
         throw new Error('Mandatory username is missing')
@@ -98,7 +99,11 @@ class Connection {
         withMemories = this._connectionArgs[ 'withMemories' ]
       }
 
-      await this._authenticate(args.username, args.password, withMemories)
+      if (this._connectionArgs.hasOwnProperty('withFeatures')) {
+        withFeatures = this._connectionArgs[ 'withFeatures' ]
+      }
+
+      await this._authenticate(args.username, args.password, withMemories, withFeatures)
     }
   }
 
@@ -249,11 +254,12 @@ class Connection {
    *
    * @private
    */
-  async _authenticate(username, password, withMemories) {
+  async _authenticate(username, password, withMemories, withFeatures = false) {
     const tokens = await this.send(this._routes[ 'authenticateByPassword' ], {
       username,
       password,
-      with_memories: withMemories
+      with_memories: withMemories,
+      with_features: withFeatures
     }, true)
 
     this._accessToken = tokens[ 'token' ]
@@ -290,8 +296,9 @@ class Connection {
    *
    * @param {boolean} forceRefresh
    * @param {boolean} withMemories
+   * @param {boolean} withFeatures
    */
-  async refreshAccessToken(forceRefresh = false, withMemories = false) {
+  async refreshAccessToken(forceRefresh = false, withMemories = false, withFeatures = false) {
     if (forceRefresh || Math.floor(Date.now() / 1000) + 10 > this._accessExpireTimestamp) {
 
       const user = this.getUser()
@@ -299,7 +306,8 @@ class Connection {
       const tokens = await this.send(this._routes[ 'refresh' ], {
         user_id: user.id,
         access_token: this._refreshToken,
-        with_memories: withMemories
+        with_memories: withMemories,
+        with_features: withFeatures
       }, true)
 
       this._user = tokens[ 'user' ]
