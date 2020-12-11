fe0578c3b685c24468cb1d94bbedb98b02f8ba5b
Nils Langner <nils.langner@leankoala.com>
2020-12-11 15:34:37 +0100

    get users; get open invitations

--- src/Repository/Entities/InvitationRepository.js
+++ src/Repository/Entities/InvitationRepository.js
@@ -22,10 +22,23 @@ class InvitationRepository extends Repository {
    * @param {Number} args.inviter The inviters user id.
    */
   async invite(project, args) {
-    const route = { path: 'user/invite/{project}', method: 'POST', version: 1 }
+    const route = { path: 'user/invitation/invite/{project}', method: 'POST', version: 1 }
     const argList = Object.assign({ project }, args)
     const requiredArguments = ['email', 'user_role', 'inviter']
     this._assertValidArguments(requiredArguments, argList)
+
+    return this._connection.send(route, argList)
+  }
+
+  /**
+   * This endpoint returns a list of all open invitations for the given project.
+   *
+   * @param project
+   * @param {Object} args
+   */
+  async getOpenInvitations(project, args) {
+    const route = { path: 'user/invitation/open/{project}', method: 'GET', version: 1 }
+    const argList = Object.assign({ project }, args)
 
     return this._connection.send(route, argList)
   }
--- src/Repository/Entities/ProjectRepository.js
+++ src/Repository/Entities/ProjectRepository.js
@@ -1,11 +1,13 @@
 const Repository = require('../Repository')
 
+
+
 /**
  * This class was created by the LeanApiBundle.
  *
  * All changes made in this file will be overwritten by the next create run.
  *
- * @created 2020-08-14
+ * @created 2020-12-11
  */
 class ProjectRepository extends Repository {
 
@@ -18,6 +20,8 @@ class ProjectRepository extends Repository {
    *                                          the value is the time in seconds till the next run (default: false)
    * @param {Boolean} args.with_onboarding_status If true the projects onboarding status is added to the
    *                                              response. (default: false)
+   * @param {Boolean} args.with_features If true the projects marketplace features are added to the
+   *                                     response. (default: false)
    */
   async search(args) {
     const route = { path: 'project/projects/search', method: 'POST', version: 1 }
@@ -52,6 +56,19 @@ class ProjectRepository extends Repository {
    */
   async delete(project, args) {
     const route = { path: 'project/projects/{project}', method: 'DELETE', version: 1 }
+    const argList = Object.assign({ project }, args)
+
+    return this._connection.send(route, argList)
+  }
+
+  /**
+   * Return all users for the given project.
+   *
+   * @param project
+   * @param {Object} args
+   */
+  async getUsers(project, args) {
+    const route = { path: 'project/users/{project}', method: 'GET', version: 1 }
     const argList = Object.assign({ project }, args)
 
     return this._connection.send(route, argList)
