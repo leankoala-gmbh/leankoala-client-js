321e0b4ff4fba6cf32bca66ca0f0f75d4979973e
Nils Langner <nils.langner@leankoala.com>
2021-01-07 18:10:21 +0100

    manage feature components

--- src/Repository/Entities/MarketplaceRepository.js
+++ src/Repository/Entities/MarketplaceRepository.js
@@ -7,7 +7,7 @@ const Repository = require('../Repository')
  *
  * All changes made in this file will be overwritten by the next create run.
  *
- * @created 2021-01-04
+ * @created 2021-01-07
  */
 class MarketplaceRepository extends Repository {
 
@@ -40,13 +40,30 @@ class MarketplaceRepository extends Repository {
   /**
    * Return all component suggestions that are enabled by the active features.
    *
-   * @param project
+   * @param system
    * @param featureIdentifier
    * @param {Object} args
    */
-  async getAdditionalComponentSuggestions(project, featureIdentifier, args) {
-    const route = { path: 'marketplace/features/suggestions/{project}/{featureIdentifier}', method: 'GET', version: 1 }
-    const argList = Object.assign({ project, featureIdentifier }, args)
+  async getComponents(system, featureIdentifier, args) {
+    const route = { path: 'marketplace/features/components/{system}/{featureIdentifier}', method: 'GET', version: 1 }
+    const argList = Object.assign({ system, featureIdentifier }, args)
+
+    return this._connection.send(route, argList)
+  }
+
+  /**
+   * Set a component using a given suggestion as template.
+   *
+   * @param system
+   * @param suggestionIdentifier
+   * @param {Object} args
+   * @param {String} args.url 
+   */
+  async setComponent(system, suggestionIdentifier, args) {
+    const route = { path: 'marketplace/features/components/{system}/{suggestionIdentifier}', method: 'POST', version: 1 }
+    const argList = Object.assign({ system, suggestionIdentifier }, args)
+    const requiredArguments = ['url']
+    this._assertValidArguments(requiredArguments, argList)
 
     return this._connection.send(route, argList)
   }
@@ -61,6 +78,37 @@ class MarketplaceRepository extends Repository {
   async getHealthStatus(system, featureIdentifier, args) {
     const route = { path: 'marketplace/features/status/{system}/{featureIdentifier}', method: 'GET', version: 1 }
     const argList = Object.assign({ system, featureIdentifier }, args)
+
+    return this._connection.send(route, argList)
+  }
+
+  /**
+   * Return all features that can be activated for the given company and provider.
+   *
+   * @param providerIdentifier
+   * @param company
+   * @param {Object} args
+   */
+  async getFeatures(providerIdentifier, company, args) {
+    const route = { path: 'marketplace/marketplace/features/{providerIdentifier}/{company}', method: 'GET', version: 1 }
+    const argList = Object.assign({ providerIdentifier, company }, args)
+
+    return this._connection.send(route, argList)
+  }
+
+  /**
+   * Activate the given feature for the given projects.
+   *
+   * @param company
+   * @param featureIdentifier
+   * @param {Object} args
+   * @param {Array} args.projects 
+   */
+  async activateFeatures(company, featureIdentifier, args) {
+    const route = { path: 'marketplace/marketplace/feature/activate/{company}/{featureIdentifier}', method: 'POST', version: 1 }
+    const argList = Object.assign({ company, featureIdentifier }, args)
+    const requiredArguments = ['projects']
+    this._assertValidArguments(requiredArguments, argList)
 
     return this._connection.send(route, argList)
   }
