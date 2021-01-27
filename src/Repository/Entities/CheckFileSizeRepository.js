76d5e4c95e6ede6256e1a8fa3fdc5c3596f638e3
Nils Langner <nils.langner@leankoala.com>
2021-01-27 15:29:28 +0100

    ignorePattern

--- src/Repository/Entities/CheckFileSizeRepository.js
+++ src/Repository/Entities/CheckFileSizeRepository.js
@@ -1,11 +1,13 @@
 const Repository = require('../Repository')
 
+
+
 /**
  * This class was created by the LeanApiBundle.
  *
  * All changes made in this file will be overwritten by the next create run.
  *
- * @created 2020-08-14
+ * @created 2021-01-27
  */
 class CheckFileSizeRepository extends Repository {
 
@@ -16,6 +18,22 @@ class CheckFileSizeRepository extends Repository {
   async getResults(system, args) {
     const route = { path: 'check/checks/{system}/performance/big', method: 'GET', version: 1 }
     const argList = Object.assign({ system }, args)
+
+    return this._connection.send(route, argList)
+  }
+
+  /**
+   * Add a new ignore pattern to the configuration.
+   *
+   * @param system
+   * @param {Object} args
+   * @param {Array} args.patterns List of URLs (strings) that will be excluded from the dead link crawl
+   */
+  async ignorePattern(system, args) {
+    const route = { path: 'check/checks/{system}/siteinfo/ignore', method: 'POST', version: 1 }
+    const argList = Object.assign({ system }, args)
+    const requiredArguments = ['patterns']
+    this._assertValidArguments(requiredArguments, argList)
 
     return this._connection.send(route, argList)
   }
