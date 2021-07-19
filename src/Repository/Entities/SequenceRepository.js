fda43561112eed5611e1990f7c4d5f9303466fb1
Nils Langner <nils.langner@leankoala.com>
2021-07-19 17:42:25 +0200

    create sequence

--- src/Repository/Entities/SequenceRepository.js
+++ src/Repository/Entities/SequenceRepository.js
@@ -48,6 +48,46 @@ class SequenceRepository extends Repository {
     return this._connection.send(route, argList)
   }
 
+  /**
+   * Create a new sequence.
+   *
+   * request url: /kapi/v1/sequences/{project}/sequence
+   * request method: POST
+   *
+   * @param project
+   * @param {Object} args
+   * @param {String} args.name
+   * @param {String} args.startUrl
+   * @param {Array} args.steps  (optional)
+   */
+  async createSequence(project, args) {
+    const route = { path: 'sequences/{project}/sequence', method: 'POST', version: 1 }
+    const argList = Object.assign({ project }, args)
+    const requiredArguments = ['name', 'startUrl']
+    this._assertValidArguments(requiredArguments, argList)
+
+    return this._connection.send(route, argList)
+  }
+
+  /**
+   * Update an existing sequence
+   *
+   * request url: /kapi/v1/sequences/{project}/sequence
+   * request method: PUT
+   *
+   * @param project
+   * @param {Object} args
+   * @param {String} args.name  (optional)
+   * @param {String} args.startUrl  (optional)
+   * @param {Array} args.steps  (optional)
+   */
+  async updateSequence(project, args) {
+    const route = { path: 'sequences/{project}/sequence', method: 'PUT', version: 1 }
+    const argList = Object.assign({ project }, args)
+
+    return this._connection.send(route, argList)
+  }
+
 }
 
 module.exports = SequenceRepository
